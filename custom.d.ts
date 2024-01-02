3bc572e878f237b56b8fa0eb5451799ce55bc349
f-fardin-dev <fakurian.fardin@gmail.com>
2024-01-02 22:09:08 +0330

    Declare ethereum type

---
+++ custom.d.ts
@@ -0,0 +1,15 @@
+// interface RequestArguments {
+//   method: string;
+//   params?: unknown[] | object;
+// }
+
+interface Window {
+  ethereum: any;
+  // {
+  //   isMetaMask:boolean;
+  //   isConnected : () => boolean;
+  //   request:(args: RequestArguments)=> Promise<unknown>;
+  //   on: (event:string, callback:(arg:any)=> void) => void;
+  //   removeListener: (event:string, callback:(arg:any)=> void) => void;
+  // };
+}
