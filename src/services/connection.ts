b51724638b47275491bb8a025e8d21f164162483
f-fardin-dev <fakurian.fardin@gmail.com>
2024-01-03 12:49:33 +0330

    Add utils for adding task

--- src/services/connection.ts
+++ src/services/connection.ts
@@ -1,3 +1,6 @@
+import { ethers } from "ethers";
+import abi from "../../blockChain/abi.json";
+
 export const connectWallet = async (): Promise<boolean | string> => {
   try {
     if (typeof window === undefined || !window?.ethereum) {
@@ -16,6 +19,34 @@ export const connectWallet = async (): Promise<boolean | string> => {
       return false;
     }
     return account[0];
+  } catch (error) {
+    console.error((error as Record<string, string>).message);
+    return false;
+  }
+};
+
+export const addTask = async (title: string): Promise<boolean> => {
+  try {
+    if (typeof window === undefined || !window?.ethereum) {
+      console.error("Metamask not detected!");
+      return false;
+    }
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const TaskContract = new ethers.Contract(
+      "0x0C1e4f8a508a1bD78DBb35b3713BFeB7D83a343f",
+      abi,
+      signer
+    );
+
+    await TaskContract.addTask(title);
+
+    // TaskContract.on("taskAdded", (recipient, taskId, event) => {
+    //   console.log("taskAdded event:", recipient, taskId, event);
+    //   // Handle the event data here
+    // });
+    return true;
   } catch (error) {
     console.error((error as Record<string, string>).message);
     return false;
